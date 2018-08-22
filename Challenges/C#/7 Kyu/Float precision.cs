f2c7080063883851eceb5c899914c266293714ac
reece <consolelogreece@gmail.com>
2018-08-22 22:54:51 +0100

    More challenges

---
+++ Challenges/C#/7 Kyu/Float precision.cs
@@ -0,0 +1,10 @@
+using System;
+public class Kata
+{
+  public static double Round(double n)
+  {
+    return Math.Round(n, 2);
+  }
+}
+
+//https://www.codewars.com/kata/5143d157ceb46d6a61000001
---
+++ Challenges/Javascript/7 Kyu/Array series #4 - Nth Smallest Element.js
@@ -0,0 +1,3 @@
+let nthSmallest=(arr, pos)=>arr.sort((a,b) => a - b)[pos - 1];
+
+//https://www.codewars.com/kata/5a512f6a80eba857280000fc
