390397851c74abaabbea1d1b8a4f2bf609eb34fa
reece <consolelogreece@gmail.com>
2019-02-22 21:41:37 +0000

    added new challenge

---
+++ Challenges/C#/8 Kyu/DNA to RNA Conversion.cs
@@ -0,0 +1,8 @@
+namespace Converter {
+  public class Converter
+  {
+    public string dnaToRna(string dna) => dna.Replace("T", "U");
+  }
+}
+
+//https://www.codewars.com/kata/5556282156230d0e5e000089
